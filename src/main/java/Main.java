dfb5c568036263087104e783adf36578ee875aa6
Olgasyla <olgasyla@gmail.com>
2024-07-03 12:12:17 +0200

    2 Persons

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,16 @@ public class Main {
         olgasCar.accelerate();
         System.out.println(olgasCar.speed);
 
+        Person person1 = new Person("Olga", 40, "Female");
+        System.out.println(person1.name+ " " + person1.age + " "+ person1.gender);
+
+        Person person2 = new Person("Eva");
+        System.out.println(person2.name);
+
+        person1.introduce();
+        person2.introduce();
+
+
     }
+
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,21 @@
 public class Person {
+    String name;
+    int age;
+    String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+
+    }
+    public Person(){
+}
+    public Person(String name){
+        this.name = name;
+    }
+    public void introduce(){
+
+        System.out.println("Hello, I am: " + name + " and " + age + " years old.");
+    }
 }
